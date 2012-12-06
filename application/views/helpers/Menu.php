8d719a6b9eb8366f6c0b88fff752310d26f01950
Paweł Tobis <pawel.tobis@gmail.com>
2012-12-06 22:41:55 +0100

    Fixed bug #30: Optimized query the database corresponding to the display category and article. Path: ../application/views/helpers/menu.php

--- application/views/helpers/Menu.php
+++ application/views/helpers/Menu.php
@@ -8,7 +8,7 @@ class Zend_View_Helper_Menu extends Zend_View_Helper_Abstract
 	public function menu()
 	{
 		$Category = new Application_Model_DbTable_Category();
-		$selectCategory = $Category->select()->from('category',array('id_category', 'sub_category', 'visible_category', 'start_site_category', 'position_category', 'name_category', 'content_category', 'title_meta_category', 'keywords_meta_category', 'description_meta_category'))->where('visible_category = ?','true')->order('position_category');
+		$selectCategory = $Category->select()->from('category')->where('visible_category = ?','true')->order('position_category');
 
 		//LIST OF CATEGORIES - THE VALUE USED TO GENERATE CATEGORIES
 		$categoryMenu = $Category->fetchAll($selectCategory);
@@ -17,7 +17,7 @@ class Zend_View_Helper_Menu extends Zend_View_Helper_Abstract
 		$subcategoryMenu = $Category->fetchAll($selectCategory);
 
 		$Article = new Application_Model_DbTable_Article();
-		$selectArticle = $Article->select()->from('article',array('id_article', 'id_category', 'visible_article', 'position_article', 'name_article', 'content_article', 'title_meta_article', 'keywords_meta_article', 'description_meta_article'))->where('visible_article = ?','true')->order('position_article');
+		$selectArticle = $Article->select()->from('article')->where('visible_article = ?','true')->order('position_article');
 
 		//LIST OF ARTICLES - THE VALUE USED TO GENERATE ARTICLES
 		$articleMenu = $Article->fetchAll($selectArticle);
